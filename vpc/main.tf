f8b3dca1189cd5baa4ec640b40a03d2696c2b011
vamsi <vamsikrishna.123.vk@gmail.com>
2023-06-10 16:12:52 +0530

    added subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,30 @@
 resource "aws_vpc" "testvpc" {
     cidr_block = var.cidr 
     tags = {
-        name = "testvpc"
+        Name = "testvpc"
     }
+}
+resource "aws_subnet" "subnet1"{
+    cidr_block = var.subcidr
+    vpc_id = aws_vpc.testvpc.id 
+    tags= {
+        Name = "subnet1"
+    }
+}
+resource "aws_internet_gateway" " gateway1"{
+    vpc_id = aws_vpc.testvpc.id
+    tags={
+        Name = "gateway1"
+    }
+}
+resource "aws_route_table" "routetbl1"{
+    vpc_id = aws_vpc.testvpc.id
+    route{
+        cidr_block= "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gateway1.id
+    }
+}
+resource "aws_route_table_association" " tableasso"{
+    subnet_id= aws_subnet.subnet1.id
+    route_table_id = aws_route_table.routetbl1.id
 }
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -3,3 +3,8 @@ variable "cidr"{
     description = "This is the cidr of vpc"
     default = "192.168.0.0/24"
 }
+variable "subcidr"{
+    type = string
+    description = "this is subnet cidr"
+    default = "192.168.1.0/8"
+}
